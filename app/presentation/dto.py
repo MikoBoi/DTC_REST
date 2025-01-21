a93cda6e8e317321c67550c54dce91f2ad57af0d
MikoBoi <m1ko01115@gmail.com>
2025-01-21 19:05:26 +0500

    create order endpoint, add order, current_user dtos

--- app/presentation/dto.py
+++ app/presentation/dto.py
@@ -1,5 +1,21 @@
 from pydantic import BaseModel
+from typing import List, Optional
 
 class AuthDTO(BaseModel):
     username: str
     password: str
+
+class CurrentUserDTO(BaseModel):
+    user_id: str
+    role: str
+
+
+
+class ProductDTO(BaseModel):
+    name: str
+    price: float
+    quantity: int
+
+class OrderCreateDTO(BaseModel):
+    customer_name: str
+    products: List[ProductDTO]
--- app/presentation/routes/orders.py
+++ app/presentation/routes/orders.py
@@ -1,10 +1,20 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
+from app.application.use_cases.orders import *
+from app.infrastructure.db import get_db
+from app.infrastructure.auth.auth_dependencies import get_current_user
+from app.infrastructure.repositories.order_repository import *
+from app.presentation.dto import *
 
 router = APIRouter()
 
 @router.post("/orders")
-def create_order():
-    return
+def create_order(order_dto: OrderCreateDTO, db=Depends(get_db), current_user: CurrentUserDTO = Depends(get_current_user)):
+    order_repository = SQLAlchemyOrderRepository(db)
+    product_repository = SQLAlchemyProductRepository(db)
+    use_case = OrderUseCases(order_repository, product_repository)
+    order = use_case.create(order_dto, int(current_user["user_id"]))
+
+    return order
 
 @router.get("/orders/{order_id}")
 def get_order():
